0ead55369af8eef622ad31fc72301894938e6532
James Yoo <jyoo980@gmail.com>
2020-09-15 16:57:35 -0700

    removing extra space

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,7 +5,7 @@ javascript:(
         if (!document.getElementById("generate-button")) {
             console.stdlog = console.log.bind(console);
             console.logs = [];
-            console.log = function () {
+            console.log = function() {
                 console.logs.push(Array.from(arguments));
                 console.stdlog.apply(console, arguments);
             };
